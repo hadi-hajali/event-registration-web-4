71906dcfc492b23f40b9536375b41df817ceae6d
Shereen Hasan <s12112478@stu.najah.edu>
2026-07-07 11:30:06 +0300

    feat: events CRUD page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import { BrowserRouter, Routes, Route, Link, Navigate } from "react-router-dom";
+import CategoriesPage from "./pages/CategoriesPage";
 import EventsPage from "./pages/EventsPage";
 
 function App() {
-  return <EventsPage />;
+  return (
+    <BrowserRouter>
+      <div className="bg-gray-800 text-white p-4 flex gap-6">
+        <Link to="/categories">Categories</Link>
+        <Link to="/events">Events</Link>
+      </div>
+
+      <Routes>
+        <Route path="/" element={<Navigate to="/categories" />} />
+        <Route path="/categories" element={<CategoriesPage />} />
+        <Route path="/events" element={<EventsPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
